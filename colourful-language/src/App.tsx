f3b2675999e664fc52bcb521c14c7abb1b9db0f0
Milindi_Kodikara <milindikodikara@gmail.com>
2025-12-19 23:02:42 +1100

    Add initial wacky design

---
+++ colourful-language/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <div className="App">
+            <header className="App-header">
+              <div className="rainbow-text">
+                <span style={{color: '#e59500', fontSize: '2em'}}>C</span>
+                <span style={{color: '#e59500', fontSize: '.5em'}}>o</span>
+                <span style={{color: '#e59500', fontSize: '0.9em'}}>l</span>
+                <span style={{color: '#e59500', fontSize: '.5em'}}>O</span>
+                <span style={{color: '#e59500', fontSize: '1em'}}>U</span>
+                <span style={{color: '#e59500', fontSize: '.7em'}}>r</span>
+                <span style={{color: '#e59500', fontSize: '0.9em'}}>F</span>
+                <span style={{color: '#e59500', fontSize: '.4em'}}>u</span>
+                <span style={{color: '#e59500', fontSize: '1em'}}>L</span>
+                <span style={{color: '#002642', fontSize: '0.8em'}}>@#$%^#&$^*%</span>
+                <span style={{color: '#e59500', fontSize: '1.2em'}}>L</span>
+                <span style={{color: '#e59500', fontSize: '1em'}}>A</span>
+                <span style={{color: '#e59500', fontSize: '.6em'}}>n</span>
+                <span style={{color: '#e59500', fontSize: '0.9em'}}>g</span>
+                <span style={{color: '#e59500', fontSize: '1.25em'}}>U</span>
+                <span style={{color: '#e59500', fontSize: '.4em'}}>a</span>
+                <span style={{color: '#e59500', fontSize: '.7em'}}>G</span>
+                <span style={{color: '#e59500', fontSize: '1.5em'}}>e</span>
+                <span style={{color: '#002642', fontSize: '0.9em'}}>!!1!1</span>
+              </div>
+            </header>
+            <div className="content">COMING SOON!</div>
+            <footer className="footer"> Made with ❤️ by
+                <a href="https://github.com/Milindi-Kodikara" target="_blank">Milindi Kodikara</a> : : <a
+                    href="https://ko-fi.com/milindi" target="_blank"> Shout me a tea</a>  🍃
+            </footer>
+        </div>
+    )
+}
+
+export default App;
